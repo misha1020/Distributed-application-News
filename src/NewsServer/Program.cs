8c27e47cb52c2167b6757cc0aa2a29ada24e520d
hlopeth <yaroslav.tolov@gmail.com>
2018-11-24 15:44:55 +0500

    just sending strings

--- src/NewsServer/Program.cs
+++ src/NewsServer/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//http://opendata.permkrai.ru/opendata/list.csv
 namespace NewsServer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (RabbitMQServer mq = new RabbitMQServer())
+            {
+                mq.Start("localhost");
+                mq.MessageSend += Pr;
+                string input = Console.ReadLine();
+                while (input.ToUpper() != "Q")
+                {
+                    try
+                    {
+                        mq.Send(input);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    input = Console.ReadLine();
+                }
+                Console.WriteLine("Stopping");
+            }
+
+        }
+
+        static public void Pr(string message)
+        {
+            Console.WriteLine(" [x] Sent {0}", message);
         }
     }
 }
