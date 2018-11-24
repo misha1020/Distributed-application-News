f2a7f1b5653fcff989570bbf050c677dc1e6ad4a
misha1020 <mishaz020@mail.ru>
2018-11-24 15:48:26 +0500

    MQ: receive message

---
+++ src/Client/BinFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class BinFormatter
+    {
+        public static byte[] ToBytes<T>(T parameters)
+        {
+            MemoryStream stream = new System.IO.MemoryStream();
+            System.Runtime.Serialization.IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, parameters);
+
+            byte[] bytes = stream.GetBuffer();
+
+            return bytes;
+        }
+
+        public static T FromBytes<T>(byte[] byteArrayData)
+        {
+            T parameters;
+            using (MemoryStream stream = new MemoryStream(byteArrayData))
+            {
+                BinaryFormatter bformatter = new BinaryFormatter();
+                parameters = (T)bformatter.Deserialize(stream);
+            }
+            return parameters;
+        }
+    }
+}
---
+++ src/Client/FormClient.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class FormClient : Form
+    {
+        public static void QueueRecieve()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "news", type: "fanout");
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "news",
+                                      routingKey: "");
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    String message;
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        message = BinFormatter.FromBytes<string>(body);
+                        MessageBox.Show("Ожидание...");
+                    };
+                    channel.BasicConsume(queue: queueName,
+                                    autoAck: true,
+                                    consumer: consumer);
+                }
+            }
+        }
+
+        public FormClient()
+        {
+            InitializeComponent();
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            QueueRecieve();            
+        }
+    }
+}
