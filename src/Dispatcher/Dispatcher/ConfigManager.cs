c86a59f09ca799d7cd9497e3df96a71290babb3b
hlopeth <yaroslav.tolov@gmail.com>
2018-11-28 18:18:06 +0500

    Dispatcher config

---
+++ src/Dispatcher/Dispatcher/ConfigManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+
+namespace Dispatcher
+{
+    class ConfigManager
+    {
+        public static string Get(string key)
+        {
+            if (ConfigurationManager.AppSettings[key] != null)
+                return ConfigurationManager.AppSettings[key];
+            else
+                throw new ConfigurationErrorsException($"fielel {key} not found");
+        }
+        public static void AddOrUpdate(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                throw new ConfigurationErrorsException($"Failed to write {key}");
+            }
+        }
+    }
+}
